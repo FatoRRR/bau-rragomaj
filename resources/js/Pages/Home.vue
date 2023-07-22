ce99414042727902b336206be2f955d5751ebca1
Fatmir <fatmir.rragomaj@more-software.de>
2023-07-22 15:25:08 +0200

    Add text to home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,41 @@
 <template>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum natus aspernatur unde ipsam reprehenderit! Dolorem
-    natus, ad consectetur fugit obcaecati quisquam doloremque assumenda libero officiis saepe provident ipsam repellendus?
-    Repellat?
-  </div>
+  <section>
+    <p>Willkommen bei Bau-Rragomaj - Ihrem zuverlässigen Partner für hochwertige Bauarbeiten!</p>
+    <p>Wir sind ein erfahrenes und leidenschaftliches Bauunternehmen, das sich der Verwirklichung Ihrer Bauprojekte mit
+      Hingabe und Fachwissen verschrieben hat. Unser Team aus hochqualifizierten Experten steht Ihnen zur Seite, um Ihre
+      Visionen in die Realität umzusetzen und Ihr Bauprojekt zu einem Erfolg zu machen.</p>
+  </section>
+  <section>
+    <p>Was uns auszeichnet:</p>
+    <p>Fachkompetenz: Mit jahrelanger Erfahrung in der Bauindustrie verfügen wir über das nötige Know-how, um selbst die
+      anspruchsvollsten Projekte professionell und termingerecht zu realisieren. Unser Team aus Architekten, Ingenieuren
+      und Handwerkern arbeitet Hand in Hand, um höchste Qualität zu gewährleisten.</p>
+    <p>Individuelle Lösungen: Jedes Bauprojekt ist einzigartig und erfordert maßgeschneiderte Lösungen. Wir hören Ihnen
+      aufmerksam zu, verstehen Ihre Bedürfnisse und erarbeiten gemeinsam die optimale Strategie, um Ihre Ziele zu
+      erreichen. Dabei achten wir stets auf Budgetvorgaben und setzen Ihre Ideen detailgetreu um.</p>
+    <p>Qualität und Nachhaltigkeit: Qualität ist für uns nicht nur ein Versprechen, sondern eine Verpflichtung. Wir
+      verwenden nur erstklassige Materialien und modernste Technologien, um langlebige und energieeffiziente Bauwerke zu
+      errichten. So tragen wir auch unseren Teil zur Nachhaltigkeit und Umweltschutz bei.</p>
+    <p>Verlässlichkeit: Pünktliche Fertigstellung und Einhaltung vereinbarter Termine sind für uns selbstverständlich. Wir
+      wissen, dass Verzögerungen nicht nur kostspielig, sondern auch frustrierend sein können. Deshalb arbeiten wir mit
+      größter Effizienz, ohne Kompromisse bei der Qualität einzugehen.</p>
+  </section>
+  <section>
+    <p>Unsere Dienstleistungen umfassen:</p>
+    <ul>
+      <li>Neubau und Altbausanierung</li>
+      <li>Gewerbe- und Industriebau</li>
+      <li>Wohnungs- und Hausbau</li>
+      <li>und vieles mehr</li>
+    </ul>
+  </section>
+  <section>
+    <p>Lassen Sie uns gemeinsam Ihre Träume verwirklichen! Kontaktieren Sie uns noch heute, um Ihre Bauprojekte zu
+      besprechen und ein unverbindliches Angebot zu erhalten. Unser engagiertes Team steht Ihnen mit Rat und Tat zur
+      Seite, um Ihr Bauvorhaben erfolgreich zu gestalten.</p>
+    <p>Vertrauen Sie auf Bau-Rragomaj, wenn es um hochwertige Bauarbeiten geht. Wir freuen uns darauf, Ihnen bei Ihrem
+      nächsten Projekt zu helfen!</p>
+  </section>
 </template>
 
 <script>
